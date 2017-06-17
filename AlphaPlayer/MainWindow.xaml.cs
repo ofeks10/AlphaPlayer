a878f28580d29b43d9024c07ec1cef1591aab246
ofeks10 <ofeks10@gmail.com>
2017-06-17 18:46:32 +0300

    #Hetchalnu :poop:

--- AlphaPlayer/MainWindow.xaml.cs
+++ AlphaPlayer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -15,9 +17,6 @@ using System.Windows.Shapes;
 
 namespace AlphaPlayer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
