a8af8d847e927b5fda5937fa3d3d6dd61cbb5592
ofeks10 <ofeks10@gmail.com>
2017-06-17 20:37:05 +0300

    Added play and pause songs, added volume slider implementation and added song time slider code.

--- AlphaPlayer/MainWindow.xaml.cs
+++ AlphaPlayer/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Windows;
 using NAudio.Wave;
 using System.Timers;
+using System.Windows.Input;
 
 namespace AlphaPlayer
 {
@@ -24,6 +25,9 @@ namespace AlphaPlayer
 
             aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
             aTimer.AutoReset = true;
+
+            this.waveOutDevice.Volume = 0.5f;
+            this.VolumeSlider.Value = this.waveOutDevice.Volume * 100;
         }
 
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
@@ -49,18 +53,47 @@ namespace AlphaPlayer
             App.Current.Dispatcher.Invoke((Action)delegate
             {
                 this.CurrentTimeLabel.Content = this.reader.CurrentTime.ToString("hh\\:mm\\:ss");
+                this.SongTimeSlider.Value = (this.reader.CurrentTime.TotalMilliseconds / this.reader.TotalTime.TotalMilliseconds) * 100;
             });
         }
 
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-            this.waveOutDevice.Init(this.reader);
+            try
+            {
+                this.waveOutDevice.Init(this.reader);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Please select a song before pressing play");
+                return;
+            }
             this.waveOutDevice.Play();
-            
-            this.waveOutDevice.Volume = 0.5f;
-            this.VolumeSlider.Value = this.waveOutDevice.Volume * 100;
-
             this.aTimer.Enabled = true;
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.waveOutDevice.Stop();
+            this.aTimer.Enabled = false;
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.waveOutDevice.Volume = (float)this.VolumeSlider.Value / 100.0f;
+        }
+
+        private void SongTimeSlider_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.aTimer.Enabled = true;
+            this.waveOutDevice.Stop();
+            this.reader.CurrentTime = TimeSpan.FromMilliseconds((this.SongTimeSlider.Value / 100.0f) * this.reader.TotalTime.TotalMilliseconds);
+            this.waveOutDevice.Play();
+        }
+
+        private void SongTimeSlider_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.aTimer.Enabled = false;
         }
     }
 }
